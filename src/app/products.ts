10fbc2e44d4155a3ae1593d37757c51ac921c8b6
heloisacamboim <heloisacamboim@gmail.com>
2022-09-25 14:44:36 -0300

    Ajuste produtos

--- src/app/products.ts
+++ src/app/products.ts
@@ -8,21 +8,38 @@ export interface Product {
 export const products = [
   {
     id: 1,
-    name: 'Batom',
-    price: 799,
-    description: 'Batom vermelho'
+    name: 'Lipgloss Jelly baby',
+    price: 20,
+    description: 'Um gloss labial de alto brilho que dá volume instantâneo.',
+    imagePath: '/assets/7c628cce-5278-45e2-a016-f13ee35ad820.jpg'
   },
   {
     id: 2,
     name: 'Sombra',
-    price: 699,
-    description: 'Paleta de sombra para os olhos'
+    price: 50,
+    description: 'Paleta de sombra para os olhos',
+    imagePath: '/assets/83a43766-53dc-406c-b616-0c1d5548fdd1.jpg'
   },
   {
     id: 3,
-    name: 'Rímel',
-    price: 299,
-    description: ''
+    name: 'Lipgloss',
+    price: 30,
+    description: 'Um gloss em tons brilhantes em embalagens de edição limitada.',
+    imagePath: '/assets/102ab042-d807-4cb2-8bf7-0f464972c278.jpg'
+  },
+  {
+    id: 4,
+    name: 'Porta Pincéis',
+    price: 60,
+    description: 'O Porta Pincéis foi desenvolvido especialmente para guardar os seus pincéis de maquiagem.',
+    imagePath: '/assets/660ccfcc-2dc0-46ca-9bc6-11ef1e1a11c0.jpg'
+  },
+  {
+    id: 5,
+    name: 'Batom',
+    price: 50,
+    description: 'Uma fórmula de batom rica e cremosa com alta pigmentação em um acabamento matte.',
+    imagePath: '/assets/6225d5d4-02da-42d7-900d-9c76a9a9fca7.jpg'
   }
 ];
 
